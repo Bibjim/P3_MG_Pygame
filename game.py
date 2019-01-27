27e2abe0b35c1326020050b5924fbd6238c210e3
Bibjim <biboujim@gmail.com.com>
2019-01-28 00:10:01 +0100

    Change name App_Lab.py to game.py

--- game.py
+++ game.py
@@ -4,3 +4,9 @@
 """MacGyver Escape Game.
 The principle of the game is to retrieve objects inside a labyrinth to lull
 the guardian to escape this one."""
+
+import pygame
+from pygame.locals import *
+
+from package.labyrinth import *
+from package.constantes import *
