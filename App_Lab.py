aab78ee47703f6b988c83cb56bc6c147d7a914bd
Bourgeois Jimi <bourgeoisjimi@MacBook-Air-de-Bourgeois.local>
2019-01-27 13:22:47 +0100

    delected App.py to change to App_lab.py

---
+++ App_Lab.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+"""MacGyver Escape Game.
+The principle of the game is to retrieve objects inside a labyrinth to lull
+the guardian to escape this one."""
